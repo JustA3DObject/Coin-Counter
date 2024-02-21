b95fd5164fb9c9c325dd7f063453be370053ec5e
Aayush <just.another.3d.object@gmail.com>
2024-02-21 21:50:11 +0530

    Added individual coins and txt with their approx area values

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import cvzone
 import numpy as np
 
-img = cv2.imread("test_images\\20240221_202734.jpg")
+img = cv2.imread("test_images\\20240221_202713.jpg")
 img = cv2.resize(img, (800,600))
 
 # def empty(a):
@@ -28,7 +28,16 @@ def preProcessing(img):
 imgPre = preProcessing(img)
 imgContours, conFound = cvzone.findContours(img, imgPre, minArea=20)
 
+if conFound:
+    for contour in conFound:
+        peri = cv2.arcLength(contour['cnt'], True)
+        approx = cv2.approxPolyDP(contour['cnt'], 0.02*peri, True)
+
+        if len(approx) > 6:
+            print(contour['area'])
+
 stackedImage = cvzone.stackImages([img, imgPre, imgContours],2,0.5)
-cv2.imshow("Imgage", stackedImage)
+cv2.imshow("Image", stackedImage)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
