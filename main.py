044d4d39c5d44b2d7ab49065381b497918164898
Aayush <just.another.3d.object@gmail.com>
2024-02-21 19:57:39 +0530

    Applied GaussianBlur

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import cv2
+import cvzone
 
 img = cv2.imread("test_images\\20240221_192151.jpg")
+img = cv2.resize(img, (800,600))
 
-cv2.namedWindow("Resized_Window", cv2.WINDOW_NORMAL) 
-cv2.resizeWindow("Resized_Window", 800, 600) 
+def preProcessing(img):
+    imgPre = cv2.GaussianBlur(img, (5,5), 4)
+    return imgPre
 
-cv2.imshow("Resized_Window", img)
+imgPre = preProcessing(img)
+
+cv2.imshow("Img", img)
+cv2.imshow("ImgPre", imgPre)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
